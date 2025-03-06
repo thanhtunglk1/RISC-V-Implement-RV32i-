cfad3fca77669241e413d2a1072c2a86919abb1b
Tung Nguyen <thanhtunglk1@gmail.com>
2025-03-06 12:23:58 +0700

    Update immgen.sv

--- Singlecycle/00_src ver 8kb dmem/immgen.sv
+++ Singlecycle/00_src ver 8kb dmem/immgen.sv
@@ -15,18 +15,18 @@ localparam //R_TYPE = 5'b01100, //don't care
 always_comb begin: proc_imm_gen
  case(i_inst[6:2])
   //I type: sign [31:25]
-  I_TYPE: o_imm = {{21{i_inst[31]}}, i_inst[30:20]};
-  I_LOAD: o_imm = {{21{i_inst[31]}}, i_inst[30:20]};
-  JALR  : o_imm = {{21{i_inst[31]}}, i_inst[30:20]};
+  : o_imm = {{21{i_inst[31]}}, i_inst[30:20]};
+  : o_imm = {{21{i_inst[31]}}, i_inst[30:20]};
+    : o_imm = {{21{i_inst[31]}}, i_inst[30:20]};
 
-  S_TYPE: o_imm = {{21{i_inst[31]}}, i_inst[30:25], i_inst[11:7]};
+  : o_imm = {{21{i_inst[31]}}, i_inst[30:25], i_inst[11:7]};
 
-  B_TYPE: o_imm = {{20{i_inst[31]}}, i_inst[7], i_inst[30:25], i_inst[11:8],1'b0};
-  JAL   : o_imm = {{12{i_inst[31]}}, i_inst[19:12], i_inst[20], i_inst[30:21],1'b0};
+  : o_imm = {{20{i_inst[31]}}, i_inst[7], i_inst[30:25], i_inst[11:8],1'b0};
+     : o_imm = {{12{i_inst[31]}}, i_inst[19:12], i_inst[20], i_inst[30:21],1'b0};
 
   //shift type: [31:12] << 12
-  LUI   : o_imm = {i_inst[31:12], 12'b0};
-  AUIPC : o_imm = {i_inst[31:12], 12'b0};
+     : o_imm = {i_inst[31:12], 12'b0};
+   : o_imm = {i_inst[31:12], 12'b0};
   
   default: o_imm = 32'b0;
  endcase
