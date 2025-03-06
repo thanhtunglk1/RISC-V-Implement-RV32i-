0e041afabc31b53376a3b26b5198e8993475dc65
Tung Nguyen <thanhtunglk1@gmail.com>
2025-03-06 12:21:33 +0700

    Update inst_mem.sv

--- Singlecycle/00_src ver 8kb dmem/inst_mem.sv
+++ Singlecycle/00_src ver 8kb dmem/inst_mem.sv
@@ -3,12 +3,12 @@ module inst_mem(
   output logic [31:0] o_inst
 );
 
-logic [3:0][7:0] imem [2**11-1:0];
-
+	//logic [3:0][7:0] imem [2**11-1:0];
+	logic [3:0][4:0][7:0] imem [2**11-1:0][4:0];
 	initial begin
 		$readmemh("../02_test/dump/mem.dump", imem);
 	end
 
-	assign o_inst = imem[i_pc_addr[12:2]];
+	assign o_inst = imem[i_pc_addr[12:2]][4:0];
 
 endmodule
